60b10f01d6eea7909c633e10d9cc5d3682ba9e4d
Thiru Moorthy <thirumoorthi.r@outlook.com>
2020-12-12 10:50:02 +0530

    cookstyle fix

--- metadata.rb
+++ metadata.rb
@@ -4,11 +4,10 @@ maintainer_email 'moorthy978@gmail.com'
 license 'All Rights Reserved'
 description 'Installs/Configures gcp_agent'
 version '0.1.0'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-chef_version     ">= 14" if respond_to?(:chef_version)
-issues_url       "https://github.com/thirumoorthir/gcp_agent/issues" if respond_to?(:issues_url)
-source_url       "https://github.com/thirumoorthir/gcp_agent" if respond_to?(:source_url)
+chef_version     '>= 14'
+issues_url       'https://github.com/thirumoorthir/gcp_agent/issues'
+source_url       'https://github.com/thirumoorthir/gcp_agent'
 
-%w{redhat centos windows}.each do |os|
+%w(redhat centos windows).each do |os|
   supports os
 end
